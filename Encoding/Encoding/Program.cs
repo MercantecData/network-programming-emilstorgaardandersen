988ce63e854e4ad50a58f79f41c5ef770b5b0b89
Emil <emil@storgaardandersen.dk>
2020-05-25 09:16:26 +0200

    Init commit

---
+++ Encoding/Encoding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Encoding
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
