ccdb6b6d7802e3b0d7d1e469e87bbac511b200c5
Emil <emil@storgaardandersen.dk>
2020-05-25 09:31:34 +0200

    Converts message to byte array

--- Encoding/Encoding/Program.cs
+++ Encoding/Encoding/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Text;
 
-namespace Encoding
+namespace Encoding1
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string message = "Hej med dig";
+
+            byte[] bytes = Encoding.ASCII.GetBytes(message);
+
+            foreach (var b in bytes)
+            {
+                Console.WriteLine(b);
+            }
         }
     }
 }
