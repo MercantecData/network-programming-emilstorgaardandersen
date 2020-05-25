7772aea6d3fc9d12787fb93b7bf59f65a3e20359
Emil <emil@storgaardandersen.dk>
2020-05-25 11:22:23 +0200

    Init commit

---
+++ Encoding2/Encoding2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace Encoding2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
