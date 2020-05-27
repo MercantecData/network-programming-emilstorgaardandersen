a1a8cb03d817a28882a438f12fa5f15741741b88
Emil <emil@storgaardandersen.dk>
2020-05-27 13:50:20 +0200

    Use async
    Can send multiple messages between client and server

--- Async/Async/Client.cs
+++ Async/Async/Client.cs
@@ -1,11 +1,53 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
 namespace Async
 {
     public class Client
     {
-        public Client()
+        public Client(int port, string serverIP)
         {
+            TcpClient client = new TcpClient();
 
+            IPAddress ip = IPAddress.Parse(serverIP);
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+
+            client.Connect(endPoint);
+
+            NetworkStream stream = client.GetStream();
+            ReceiveMessage(stream);
+
+            while (true)
+            {
+                Console.Write("Write your message here: ");
+                string text = Console.ReadLine();
+                SendMessage(text, stream);
+            }
+
+            //client.Close();
+        }
+
+        public async void ReceiveMessage(NetworkStream stream)
+        {
+            byte[] buffer = new byte[256];
+
+            while (true)
+            {
+                int numberOfBytesRead = await stream.ReadAsync(buffer, 0, 256);
+                string receivedMessage = Encoding.UTF8.GetString(buffer, 0, numberOfBytesRead);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("\n" + receivedMessage);
+                Console.ResetColor();
+            }
+        }
+
+        static void SendMessage(string text, NetworkStream stream)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+            stream.Write(buffer, 0, buffer.Length);
         }
     }
 }
--- Async/Async/Server.cs
+++ Async/Async/Server.cs
@@ -1,11 +1,62 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Async
 {
     public class Server
     {
-        public Server()
+        public Server(int port)
         {
+            TcpListener listener = StartListener(port);
 
+            Console.WriteLine("Awaiting Clients...");
+            TcpClient client = listener.AcceptTcpClient();
+
+            NetworkStream stream = client.GetStream();
+            ReceiveMessage(stream);
+
+            while (true)
+            {
+                Console.Write("Write your message here: ");
+                string text = Console.ReadLine();
+                SendMessage(text, stream);
+            }
+
+            //Console.ReadKey();
+            listener.Stop();
+        }
+
+        public async void ReceiveMessage(NetworkStream stream)
+        {
+            byte[] buffer = new byte[256];
+
+            while (true)
+            {
+                int numberOfBytesRead = await stream.ReadAsync(buffer, 0, 256);
+                string receivedMessage = Encoding.UTF8.GetString(buffer, 0, numberOfBytesRead);
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("\n" + receivedMessage);
+                Console.ResetColor();
+            }
+        }
+
+        static TcpListener StartListener(int port)
+        {
+            IPAddress ip1 = IPAddress.Any;
+            IPEndPoint endpoint1 = new IPEndPoint(ip1, port);
+            TcpListener listener = new TcpListener(endpoint1);
+            listener.Start();
+            return listener;
+        }
+
+        static void SendMessage(string text, NetworkStream stream)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(text);
+            stream.Write(buffer, 0, buffer.Length);
         }
     }
 }
