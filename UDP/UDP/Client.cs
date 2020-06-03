8ac46ea02ed7116d6ea9ad123ab3537caf4d4646
Emil <emil@storgaardandersen.dk>
2020-06-03 13:21:01 +0200

    UDP
    Call response with UDP

---
+++ UDP/UDP/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace UDP
+{
+    public class Client
+    {
+        public Client(string serverIP)
+        {
+            UdpClient client = new UdpClient();
+
+            string text = "Hello UDP!";
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse(serverIP), 1234);
+
+            client.Send(bytes, bytes.Length, endpoint);
+        }
+    }
+}
---
+++ UDP/UDP/Server.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace UDP
+{
+    public class Server
+    {
+        public Server()
+        {
+            Receiver();
+            Console.ReadLine();
+        }
+
+        public static async void Receiver()
+        {
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("0.0.0.0"), 1234);
+            UdpClient client = new UdpClient(endpoint);
+
+            UdpReceiveResult result = await client.ReceiveAsync();
+
+            byte[] buffer = result.Buffer;
+
+            string text = Encoding.UTF8.GetString(buffer);
+
+            Console.WriteLine("Received: " + text);
+        }
+    }
+}
