411d7140801ca46b909b0b289999f48472684bd3
Emil <emil@storgaardandersen.dk>
2020-06-08 13:41:57 +0200

    Added comments

--- Encoding/Encoding/Program.cs
+++ Encoding/Encoding/Program.cs
@@ -9,15 +9,20 @@ namespace Encoding1
         {
             string message = "Hej med dig, håber det går godt";
 
+            // Converts message to byte array
             byte[] bytes = Encoding.UTF8.GetBytes(message);
 
+            // Loops through each byte in the byte array
             foreach (var b in bytes)
             {
+                // Prints the byte
                 Console.WriteLine(b);
             }
 
+            // Converts bytes to string
             string converted = Encoding.UTF8.GetString(bytes);
 
+            // Prints the message
             Console.WriteLine(converted);
         }
     }
